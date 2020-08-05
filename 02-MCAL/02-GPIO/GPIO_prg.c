c8c028a9d84b44e4f43d4879b27cfba44d3241fe
YousifMousa <Yousif.Mousa97@gmail.com>
2020-08-05 20:50:09 +0200

    Applying code optimization

--- 02-MCAL/02-GPIO/GPIO_prg.c
+++ 02-MCAL/02-GPIO/GPIO_prg.c
@@ -48,10 +48,15 @@ GPIO_ErrorStatusType GPIO_enuSetPinValue(u8 const u8PinNameCpy, u8 const u8PinVa
 /*	enuErrorStatusLoc is the error status of the function if any error		*/ 
 /*	happens in the function.												*/ 
     GPIO_ErrorStatusType enuErrorStatusLoc = GPIO_NO_ERRORS_FOUND;
+	
+/*	A local pointer to select which port the pin used in. It is initialized */
+/*	to GPIO port A.															*/
+    volatile GPIOType * pGPIOTypePortNameLoc = (volatile GPIOType *) GPIOA;
 
 /*	u32PinPositionLoc is used to determine which bit in the 32 bit regieter */
 /* 	we are going to change.													*/
     u32 u32PinPositionLoc = u8PinNameCpy;
+
 
 /*  Check the name is in the range of available pins in the STM32f103c8t6.	*/
     if(u8PinNameCpy > GPIO_END_PIN){
@@ -68,53 +73,42 @@ GPIO_ErrorStatusType GPIO_enuSetPinValue(u8 const u8PinNameCpy, u8 const u8PinVa
     else{
 /*  Check if the name (u8PinNameCpy) of the pin is in PORTA.				*/
         if(u8PinNameCpy <= GPIOA_END){
-/*  Check the value that is needed to be assigned to the PIN.           	*/
-            if(u8PinValueCpy == GPIO_HIGH ){
-/*	We use the BSRR Register for atomic Operations.							*/
-				 GPIOA->BSRR = 
-						FIRST_BIT_LOCATION <<u32PinPositionLoc;
-            }
-            else{
-/*  if the Value to be assigned to the value is LOW.						*/
-				 GPIOA->BRR = 
-						FIRST_BIT_LOCATION << u32PinPositionLoc;
-            }
+/*	Assigning a pointer to the GPIOA register.								*/
+			pGPIOTypePortNameLoc = (volatile GPIOType *) GPIOA;
 		}
 /*  check if the name (u8PinNameCpy) of the pin is in PORTB.				*/
-        else if(u8PinNameCpy <= GPIOB_END ){
+        else if(u8PinNameCpy <= GPIOB_END ){		
 /*  if it is in range of PORTB, we calculate the position of the pin		*/
 /* 	map register.              												*/
             u32PinPositionLoc = u8PinNameCpy % PINS_PREV_TO_GPIOB; 
-/*  Check the value that is needed to be assigned to the PIN.				*/
-			if(u8PinValueCpy == GPIO_HIGH ){
-/*	We use the BSRR Register for atomic Operations.							*/
-				 GPIOB->BSRR =
-						FIRST_BIT_LOCATION << u32PinPositionLoc;
-            }
-            else{
-/*  if the Value to be assigned to the value is LOW.						*/
-				 GPIOB->BRR = 
-						FIRST_BIT_LOCATION << u32PinPositionLoc;
-            }
-        }
-/*  check if the name (u8PinNameCpy) pin is in PORTC.						*/
+			
+/*	Assigning a pointer to the GPIOB register.								*/
+			pGPIOTypePortNameLoc = (volatile GPIOType *) GPIOB;
+		}
+		
         else{
 /*	If it is in PORTC we calculate the location of the pin relative to the 	*/
 /*	port because in stm32f103 (blue pill) there is only 3 pins in portc 	*/
 /* 	which are PIN13, PIN14, PIN15.											*/
             u32PinPositionLoc = 
 					 (u8PinNameCpy % PINS_PREV_TO_GPIOC) + GPIOC_PINS_OFFSET;
-
-/*  Check the value that is needed to be assigned to the PIN.                                   */
-			if(u8PinValueCpy == GPIO_HIGH ){
-/*	We use the BSRR Register for atomic Operations.												*/
-				 GPIOC->BSRR = FIRST_BIT_LOCATION << u32PinPositionLoc;
-			}
-            else{
-/*  if the Value to be assigned to the value is LOW.                                           */
-				 GPIOC->BRR = FIRST_BIT_LOCATION << u32PinPositionLoc;
-            }
+					 
+/*	Assigning a pointer to the GPIOC register.								*/
+			pGPIOTypePortNameLoc = (volatile GPIOType *) GPIOC;
         }
+		
+/*  Check the value that is needed to be assigned to the PIN.           	*/
+		if(u8PinValueCpy == GPIO_HIGH ){
+/*	we dereference the Pointer that assigned to the GPIO selected. And we 	*/
+/*	use the BSRR Register for atomic Operations.							*/
+			pGPIOTypePortNameLoc->BSRR = 
+					FIRST_BIT_LOCATION <<u32PinPositionLoc;
+		}
+		else{
+/* 	For LOW values we use  BRR register. */
+			pGPIOTypePortNameLoc->BRR = 
+					FIRST_BIT_LOCATION << u32PinPositionLoc;
+		}
 	}
     return enuErrorStatusLoc; 
 }
@@ -131,6 +125,9 @@ GPIO_ErrorStatusType GPIO_enuSetMode(u8 const u8PinNameCpy, u8 const u8ModeCpy )
 /*	u32PinPositionLoc is used to determine which bits in the 32  			*/
 /* 	bit-regieter we are going to change.									*/
     u32 u32PinPositionLoc = FIRST_BIT_LOCATION;
+	
+/*	u8GPIOModeLoc is used to hold the value of the GPIO Configuration.		*/
+	u8 u8GPIOModeLoc = GPO_PUSHPULL_CNF_MODE;
 
 /*  Check the name is in the range of available pins in the STM32f103c8t6.	*/
     if(u8PinNameCpy > GPIO_END_PIN){
@@ -179,198 +176,69 @@ GPIO_ErrorStatusType GPIO_enuSetMode(u8 const u8PinNameCpy, u8 const u8ModeCpy )
 		switch (u8ModeCpy){
 /*	In case its a general purpose output pin Pushpull.						*/
 			case GPIO_GPO_PUSHPULL		:
-/*	Check on the location of the pin to operate on the lower or the higher 	*/
-/*	Register.																*/
-				if (u32PinPositionLoc < LOWER_CONFIG_REG ){
-/*	Setting the necesseray bits to one in the lower config register.		*/
-					pGPIOTypePortNameLoc->CRL |= 
-							(GPO_PUSHPULL_CNF_MODE_ONES << u32PinPositionLoc);
-/*	Setting the necesseray bits to zeros in the lower config register.		*/
-					pGPIOTypePortNameLoc->CRL &= 
-							~(GPO_PUSHPULL_CNF_MODE_ZEROS << u32PinPositionLoc);
-				}
-				else{
-					u32PinPositionLoc =
-							u32PinPositionLoc %  NUMBER_OF_BITS_IN_REGISTER;
-/*	Setting the necesseray bits to one in the lower config register.		*/
-					pGPIOTypePortNameLoc->CRH |= 
-							(GPO_PUSHPULL_CNF_MODE_ONES << u32PinPositionLoc);
-/*	Setting the necesseray bits to zeros in the lower config register.		*/
-					pGPIOTypePortNameLoc->CRH &= 
-							~(GPO_PUSHPULL_CNF_MODE_ZEROS << u32PinPositionLoc);
-				}
+/* 	Assign the u8GPIOModeLoc to PushPull output mode.						*/
+				u8GPIOModeLoc = GPO_PUSHPULL_CNF_MODE;
 				break;
 
 /*	In case its a general purpose output pin opendrain.						*/
 			case GPIO_GPO_OPEN_DRAIN	:
-/*	Check on the location of the pin to operate on the lower or the higher 	*/
-/*	Register.																*/
-				if (u32PinPositionLoc < LOWER_CONFIG_REG ){
-/*	Setting the necesseray bits to one in the lower config register.		*/
-					pGPIOTypePortNameLoc->CRL |= 
-							(GPO_OPEN_DRAIN_CNF_MODE_ONES << u32PinPositionLoc);
-/*	Setting the necesseray bits to zeros in the lower config register.		*/
-					pGPIOTypePortNameLoc->CRL &= 
-							~(GPO_OPEN_DRAIN_CNF_MODE_ZEROS << u32PinPositionLoc);
-				}
-				else{
-					u32PinPositionLoc =
-							u32PinPositionLoc %  NUMBER_OF_BITS_IN_REGISTER;
-/*	Setting the necesseray bits to one in the lower config register.		*/
-					pGPIOTypePortNameLoc->CRH |= 
-							(GPO_OPEN_DRAIN_CNF_MODE_ONES << u32PinPositionLoc);
-/*	Setting the necesseray bits to zeros in the lower config register.		*/
-					pGPIOTypePortNameLoc->CRH &=
-							~(GPO_OPEN_DRAIN_CNF_MODE_ZEROS << u32PinPositionLoc);
-				}
+/* 	Assign the u8GPIOModeLoc to open Drain output mode.						*/
+				u8GPIOModeLoc = GPO_OPEN_DRAIN_CNF_MODE;
 				break;
 
 /*	In case its a alternate function output pin Pushpull.					*/
 			case GPIO_AFO_PUSHPULL	 	:
-/*	Check on the location of the pin to operate on the lower or the higher 	*/
-/*	Register.																*/
-				if (u32PinPositionLoc < LOWER_CONFIG_REG ){
-/*	Setting the necesseray bits to one in the lower config register.		*/
-					pGPIOTypePortNameLoc->CRL |= 
-							(AFO_PUSHPULL_CNF_MODE_ONES << u32PinPositionLoc);
-/*	Setting the necesseray bits to zeros in the lower config register.		*/
-					pGPIOTypePortNameLoc->CRL &= 
-							~(AFO_PUSHPULL_CNF_MODE_ZEROS << u32PinPositionLoc);
-				}
-				else{
-					u32PinPositionLoc =
-							u32PinPositionLoc %  NUMBER_OF_BITS_IN_REGISTER;
-/*	Setting the necesseray bits to one in the lower config register.		*/
-					pGPIOTypePortNameLoc->CRH |= 
-							(AFO_PUSHPULL_CNF_MODE_ONES << u32PinPositionLoc);
-/*	Setting the necesseray bits to zeros in the lower config register.		*/
-					pGPIOTypePortNameLoc->CRH &=
-							~(AFO_PUSHPULL_CNF_MODE_ZEROS << u32PinPositionLoc);
-				}
+/* 	Assign the u8GPIOModeLoc to  alternate function mode Pushpull. 			*/
+				u8GPIOModeLoc = AFO_PUSHPULL_CNF_MODE;	
 				break;
 
-
-/*	In case its a alternate function output pin opendrain.					*/
+/*	In case its a alternate function output pin openc drain.				*/
 			case GPIO_AFO_OPEN_DRAIN	:
-/*	Check on the location of the pin to operate on the lower or the higher 	*/
-/*	Register.																*/
-				if (u32PinPositionLoc < LOWER_CONFIG_REG ){
-/*	Setting the necesseray bits to one in the lower config register.		*/
-					pGPIOTypePortNameLoc->CRL |= 
-							(AFO_OPEN_DRAIN_CNF_MODE_ONES << u32PinPositionLoc);
-/*	Setting the necesseray bits to zeros in the lower config register.		*/
-					pGPIOTypePortNameLoc->CRL &= 
-							~(AFO_OPEN_DRAIN_CNF_MODE_ZEROS << u32PinPositionLoc);
-				}
-				else{
-					u32PinPositionLoc = 
-							u32PinPositionLoc %  NUMBER_OF_BITS_IN_REGISTER;
-/*	Setting the necesseray bits to one in the lower config register.		*/
-					pGPIOTypePortNameLoc->CRH |= 
-							(AFO_OPEN_DRAIN_CNF_MODE_ONES << u32PinPositionLoc);
-/*	Setting the necesseray bits to zeros in the lower config register.		*/
-					pGPIOTypePortNameLoc->CRH &=
-							~(AFO_OPEN_DRAIN_CNF_MODE_ZEROS << u32PinPositionLoc);
-				}
+/* 	Assign the u8GPIOModeLoc to open drain alternate function mode.			*/
+				u8GPIOModeLoc = AFO_OPEN_DRAIN_CNF_MODE;	
 				break;
  
-
 /*	In case its a analog input pin.											*/
 			case GPIO_INPUT_ANALOG	 	:
-/*	Check on the location of the pin to operate on the lower or the higher 	*/
-/*	Register.																*/
-				if (u32PinPositionLoc < LOWER_CONFIG_REG ){
-/*	Setting the necesseray bits to one in the lower config register.		*/
-					pGPIOTypePortNameLoc->CRL |= 
-							(INPUT_ANALOG_CNF_MODE_ONES << u32PinPositionLoc);
-/*	Setting the necesseray bits to zeros in the lower config register.		*/
-					pGPIOTypePortNameLoc->CRL &= 
-							~(INPUT_ANALOG_CNF_MODE_ZEROS << u32PinPositionLoc);
-				}
-				else{
-					u32PinPositionLoc = 
-							u32PinPositionLoc %  NUMBER_OF_BITS_IN_REGISTER;
-/*	Setting the necesseray bits to one in the lower config register.		*/
-					pGPIOTypePortNameLoc->CRH |= 
-							(INPUT_ANALOG_CNF_MODE_ONES << u32PinPositionLoc);
-/*	Setting the necesseray bits to zeros in the lower config register.		*/
-					pGPIOTypePortNameLoc->CRH &=
-							~(INPUT_ANALOG_CNF_MODE_ZEROS << u32PinPositionLoc);
-				}
+/* 	Assign the u8GPIOModeLoc to analog input  mode.							*/
+				u8GPIOModeLoc = INPUT_ANALOG_CNF_MODE;	
 				break;
 
 /*	In case its a analog input floating pin.								*/
 			case GPIO_INPUT_FLOATING	:
-/*	Check on the location of the pin to operate on the lower or the higher 	*/
-/*	Register.																*/
-				if (u32PinPositionLoc < LOWER_CONFIG_REG ){
-/*	Setting the necesseray bits to one in the lower config register.		*/
-					pGPIOTypePortNameLoc->CRL |= 
-							(INPUT_FLOATING_CNF_MODE_ONES << u32PinPositionLoc);
-/*	Setting the necesseray bits to zeros in the lower config register.		*/
-					pGPIOTypePortNameLoc->CRL &= 
-							~(INPUT_FLOATING_CNF_MODE_ZEROS << u32PinPositionLoc);
-				}
-				else{
-					u32PinPositionLoc = 
-							u32PinPositionLoc %  NUMBER_OF_BITS_IN_REGISTER;
-/*	Setting the necesseray bits to one in the lower config register.		*/
-					pGPIOTypePortNameLoc->CRH |= 
-							(INPUT_FLOATING_CNF_MODE_ONES << u32PinPositionLoc);
-/*	Setting the necesseray bits to zeros in the lower config register.		*/
-					pGPIOTypePortNameLoc->CRH &=
-							~(INPUT_FLOATING_CNF_MODE_ZEROS << u32PinPositionLoc);
-				}
-				 break;
-
-/*	In case it is an analog input pulldown pin.								*/
-			case GPIO_INPUT_PULLDOWN	:
-/*	Check on the location of the pin to operate on the lower or the higher 	*/
-/*	Register.																*/
-				if (u32PinPositionLoc < LOWER_CONFIG_REG ){
-/*	Setting the necesseray bits to one in the lower config register.		*/
-					pGPIOTypePortNameLoc->CRL |= 
-							(INPUT_PULLDOWN_CNF_MODE_ONES << u32PinPositionLoc);
-/*	Setting the necesseray bits to zeros in the lower config register.		*/
-					pGPIOTypePortNameLoc->CRL &= 
-							~(INPUT_PULLDOWN_CNF_MODE_ZEROS << u32PinPositionLoc);
-				}
-				else{
-					u32PinPositionLoc = 
-							u32PinPositionLoc %  NUMBER_OF_BITS_IN_REGISTER;
-/*	Setting the necesseray bits to one in the lower config register.		*/
-					pGPIOTypePortNameLoc->CRH |= 
-							(INPUT_PULLDOWN_CNF_MODE_ONES << u32PinPositionLoc);
-/*	Setting the necesseray bits to zeros in the lower config register.		*/
-					pGPIOTypePortNameLoc->CRH &=
-							~(INPUT_PULLDOWN_CNF_MODE_ZEROS << u32PinPositionLoc);
-				}
+/* 	Assign the u8GPIOModeLoc to  input floating mode.						*/
+				u8GPIOModeLoc = INPUT_FLOATING_CNF_MODE;	
 				break;
-	
 	
 /*	In case it is an analog input pullup pin OR input pulldown				*/
 			default		:
-/*	Check on the location of the pin to operate on the lower or the higher 	*/
-/*	Register.																*/
-				if (u32PinPositionLoc < LOWER_CONFIG_REG ){
-/*	Setting the necesseray bits to one in the lower config register.		*/
-					pGPIOTypePortNameLoc->CRL |= 
-							(INPUT_PULLUP_CNF_MODE_ONES << u32PinPositionLoc);
-/*	Setting the necesseray bits to zeros in the lower config register.		*/
-					pGPIOTypePortNameLoc->CRL &= 
-							~(INPUT_PULLUP_CNF_MODE_ZEROS << u32PinPositionLoc);
-				}
-				else{
-					u32PinPositionLoc = 
-							u32PinPositionLoc %  NUMBER_OF_BITS_IN_REGISTER;
-/*	Setting the necesseray bits to one in the lower config register.		*/
-					pGPIOTypePortNameLoc->CRH |= 
-							(INPUT_PULLUP_CNF_MODE_ONES << u32PinPositionLoc);
-/*	Setting the necesseray bits to zeros in the lower config register.		*/
-					pGPIOTypePortNameLoc->CRH &=
-							~(INPUT_PULLUP_CNF_MODE_ZEROS << u32PinPositionLoc);
-				}
+/* 	Assign the u8GPIOModeLoc to pullup pin OR input pulldown.				*/
+				u8GPIOModeLoc = INPUT_PULLDOWN_PULLUP_CNF_MODE;	
 				break;			
+		}
+		/*	Check on the location of the pin to operate on the lower or the higher 	*/
+/*	Register.																*/
+		if (u32PinPositionLoc < LOWER_CONFIG_REG ){
+/*	Setting the necesseray bits to one in the lower config register.		*/
+			pGPIOTypePortNameLoc->CRL |= 
+						(u8GPIOModeLoc << u32PinPositionLoc);
+/*	Get the zeros in the 4 bit configration bits.							*/
+			u8GPIOModeLoc = (~u8GPIOModeLoc) & FIRST_FOUR_BIT_MASK;
+/*	Setting the necesseray bits to zeros in the lower config register.		*/
+			pGPIOTypePortNameLoc->CRL &= 
+						~(u8GPIOModeLoc << u32PinPositionLoc);
+		}
+		else{
+			u32PinPositionLoc =
+						u32PinPositionLoc %  NUMBER_OF_BITS_IN_REGISTER;
+/*	Setting the necesseray bits to one in the lower config register.		*/
+			pGPIOTypePortNameLoc->CRH |= 
+						(u8GPIOModeLoc << u32PinPositionLoc);		
+/*	Get the zeros in the 4 bit configration bits.							*/
+			u8GPIOModeLoc = (~u8GPIOModeLoc) & FIRST_FOUR_BIT_MASK;
+/*	Setting the necesseray bits to zeros in the lower config register.		*/
+			pGPIOTypePortNameLoc->CRL &= 
+						~(u8GPIOModeLoc << u32PinPositionLoc);
 		}
     }
     return enuErrorStatusLoc; 
